aad9c024524e8100c40375fa8b942cc780c93109
mucacran <el.mucacran.rasta@hotmail.com>
2023-02-25 20:43:11 -0500

    Agregar funcionalidad de autenticación de usuarios

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require ('express');
+const app = express();
+require('dotenv').config();
+
+const port = process.env.PORT
+app.listen(port,()=>{
+    console.log(`escuchando en el puerto ${port} pilas`);
+});
+
+// index.js
+const { auth,requiresAuth } = require('express-openid-connect');
+app.use(
+  auth({
+    authRequired: false,
+    auth0Logout: true,
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    baseURL: process.env.BASE_URL+port,
+    secret: process.env.SECRET,
+    idpLogout: true,
+  })
+);
+
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
+}) ;
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
+//https://github.com/auth0/express-openid-connect/blob/master/EXAMPLES.md
